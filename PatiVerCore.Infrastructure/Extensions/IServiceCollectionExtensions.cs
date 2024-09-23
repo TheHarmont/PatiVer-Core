734d335c2ad4c235897c5decda2932067614d840
GolikovVI <GolikovVI@miac.medkhv.ru>
2024-09-23 17:08:50 +1000

    Настроены ServiceCollectionExtensions

---
+++ PatiVerCore.Infrastructure/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PatiVerCore.Application.Interfaces;
+using PatiVerCore.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatiVerCore.Infrastructure.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        private const string redisSectionName = "Redis";
+        private const string configSectionName = "Configuration";
+        private const string instanceSectionName = "InstanceName";
+
+        public static void AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRedis(configuration);
+            services.AddServices();
+        }
+
+        private static void AddRedis(this IServiceCollection services, IConfiguration configuration)
+        {
+            var redisConfig = configuration.GetSection(redisSectionName);
+            var config = redisConfig[configSectionName];
+            var instanceName = redisConfig[instanceSectionName];
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = config;
+                options.InstanceName = instanceName;
+            });
+        }
+
+        private static void AddServices(this IServiceCollection services)
+        {
+            services
+                .AddTransient<ICacheService, CacheService>();
+        }
+    }
+}
---
+++ PatiVerCore.Persistence/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PatiVerCore.Application.Interfaces.Repositories;
+using PatiVerCore.Persistence.Context;
+using PatiVerCore.Persistence.Repositories;
+
+namespace PatiVerCore.Persistence.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        private const string connectionStringSectionName = "PatiVer";
+
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext(configuration);
+            services.AddRepositories();
+        }
+
+        public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString(connectionStringSectionName);
+
+            services.AddDbContext<AppDBContext>(options =>
+               options.UseSqlServer(connectionString,
+                   builder => builder.MigrationsAssembly(typeof(AppDBContext).Assembly.FullName)));
+        }
+
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.
+                AddScoped<ILocalDataRepository, LocalDataRepository>();
+        }
+    }
+}

4f44f5e52414bbc0d9e16f989580ab25804b8e5a
GolikovVI <GolikovVI@miac.medkhv.ru>
2024-09-23 17:12:05 +1000

    Подготовлен LocalDaraRepository

---
+++ PatiVerCore.Application/Interfaces/Repositories/ILocalDataRepository.cs
@@ -0,0 +1,43 @@
+﻿using PatiVerCore.Domain.Common.Result;
+using PatiVerCore.Domain.Entities;
+using PatiVerCore.Domain.Entities.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatiVerCore.Application.Interfaces.Repositories
+{
+    public interface ILocalDataRepository
+    {
+        /// <summary>
+        /// Возвращает данные пациента из БД, отфильтрованные по входным данным.
+        /// </summary>
+        /// <param name="firstName">Имя</param>
+        /// <param name="lastName">Фамилия</param>
+        /// <param name="patronymic">Отчество</param>
+        /// <param name="birthDate">Дата рождения</param>
+        /// <returns>
+        /// 
+        /// </returns>
+        public Task<Result<LocalData>> GetLocalDataByFioAsync(PersonFIO data);
+
+        /// <summary>
+        /// Возвращает данные пациента из БД, отфильтрованные по входным данным.
+        /// </summary>
+        /// <param name="polis">Полис</param>
+        /// <returns>
+        /// <see cref="LocalData"/>
+        public Task<Result<LocalData>> GetDataByPolisAsync(PersonPolis data);
+
+        /// <summary>
+        /// Возвращает данные пациента из БД, отфильтрованные по входным данным.
+        /// </summary>
+        /// <param name="snils">Снилс</param>
+        /// <returns>
+        /// <see cref="LocalData"/>
+        public Task<Result<LocalData>> GetDataBySnilsAsync(PersonSnils data);
+
+    }
+}
---
+++ PatiVerCore.Domain/Common/BaseRequestEnity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatiVerCore.Domain.Common
+{
+    public abstract class BaseRequestEnity
+    {
+        /// <summary>
+        /// Идентификатор МО
+        /// </summary>
+        public string? MoId { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string? Username { get; set; }
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsIPRAfirst { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? MIS { get; set; }
+
+        public BaseRequestEnity(string moId, string username, string password, bool isIPRAfirst, int mis)
+        {
+            MoId = moId;
+            Username = username;
+            Password = password;
+            IsIPRAfirst = isIPRAfirst;
+            MIS = mis;
+        }
+    }
+}
